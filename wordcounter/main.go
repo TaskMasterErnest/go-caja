bd641ef99ebef50f289ad039268517ee26913f7b
TaskMasterErnest <kluernest08@gmail.com>
2024-08-28 08:41:38 +0000

    added line and byte counter, added tests

--- wordcounter/main.go
+++ wordcounter/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// read text from input
 	scanner := bufio.NewScanner(r)
-	// scan the words in the text given
-	scanner.Split(bufio.ScanWords)
+	// check if the countLines is enabled
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}
 	// initialize the word counter variable
 	var wc int
 	// count the words, increment counter and print
@@ -22,5 +28,11 @@ func count(r io.Reader) int {
 }
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	// initialize flags
+	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
+	flag.Parse()
+
+	// print output with the flags enabled
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
--- wordcounter/main_test.go
+++ wordcounter/main_test.go
@@ -12,9 +12,27 @@ func TestCountWords(t *testing.T) {
 	// expected outcome
 	expected := 5
 	// call the count function
-	result := count(buf)
+	result := count(buf, false, false)
 
 	if result != expected {
-		t.Errorf("Expected %d, got %d instead", expected, result)
+		t.Errorf("Expected %d, got %d instead\n", expected, result)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	buf := bytes.NewBufferString("word1 word2\n word3\n word4 word5")
+	expected := 3
+	result := count(buf, true, false)
+	if result != expected {
+		t.Errorf("Expected %d, got %d instead\n", expected, result)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	buf := bytes.NewBufferString("word1 word2 word3 word4 word5")
+	expected := 29
+	result := count(buf, false, true)
+	if result != expected {
+		t.Errorf("Expected %d, got %d instead\n", expected, result)
 	}
 }
