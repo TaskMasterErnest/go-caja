030f251f3ad76aac9c59cbd65a806b97fdcd1049
TaskMasterErnest <kluernest08@gmail.com>
2024-08-29 08:12:18 +0000

    added flags, added tests on flags

--- todo/cmd/todo/main.go
+++ todo/cmd/todo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/go-caja/todo"
 )
@@ -11,6 +11,12 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	// adding flags for true CLI implementation
+	task := flag.String("task", "", "task to be included in ToDo list")
+	list := flag.Bool("list", false, "list all tasks.")
+	complete := flag.Int("complete", 0, "item to the marked as complete")
+	flag.Parse()
+
 	l := &todo.List{}
 
 	if err := l.Get(todoFileName); err != nil {
@@ -19,16 +25,31 @@ func main() {
 	}
 
 	switch {
-	case len(os.Args) == 1:
-		for _, item := range *l {
-			fmt.Println(item.Task)
+	case *list:
+		for _, task := range *l {
+			if !task.Done {
+				fmt.Println(task.Task)
+			}
 		}
-	default:
-		item := strings.Join(os.Args[1:], " ")
-		l.Add(item)
+	case *complete > 0:
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		// save the file
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	case *task != "":
+		l.Add(*task)
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 }
