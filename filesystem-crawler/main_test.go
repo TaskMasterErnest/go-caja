47d6c678ce30e0c938e83c40f06ba81cae4a35e2
TaskMasterErnest <kluernest08@gmail.com>
2024-09-18 10:03:30 +0000

    adding testing for mutiple extensions, help needed

--- filesystem-crawler/main_test.go
+++ filesystem-crawler/main_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -15,15 +16,15 @@ func TestRun(t *testing.T) {
 		cfg      config
 		expected string
 	}{
-		{name: "NoFilter", root: "testdata", cfg: config{ext: "", size: 0, list: true},
+		{name: "NoFilter", root: "testdata", cfg: config{ext: []string{""}, size: 0, list: true},
 			expected: "testdata/dir.log\ntestdata/dir2/script.sh\n"},
-		{name: "FilterExtensionMatch", root: "testdata", cfg: config{ext: ".log", size: 0, list: true},
+		{name: "FilterExtensionMatch", root: "testdata", cfg: config{ext: []string{".log"}, size: 0, list: true},
 			expected: "testdata/dir.log\n"},
-		{name: "FilterExtensionSizeMatch", root: "testdata", cfg: config{ext: ".log", size: 10, list: true},
+		{name: "FilterExtensionSizeMatch", root: "testdata", cfg: config{ext: []string{".log"}, size: 10, list: true},
 			expected: "testdata/dir.log\n"},
-		{name: "FilterExtensionSizeNoMatch", root: "testdata", cfg: config{ext: ".log", size: 20, list: true},
+		{name: "FilterExtensionSizeNoMatch", root: "testdata", cfg: config{ext: []string{".log"}, size: 20, list: true},
 			expected: ""},
-		{name: "FilterExtensionNoMatch", root: "testdata", cfg: config{ext: ".gz", size: 0, list: true},
+		{name: "FilterExtensionNoMatch", root: "testdata", cfg: config{ext: []string{".gz"}, size: 0, list: true},
 			expected: ""},
 	}
 
@@ -75,11 +76,11 @@ func TestRunDelExtension(t *testing.T) {
 		nNoDelete   int
 		expected    string
 	}{
-		{name: "DeleteExtensionNoMatch", cfg: config{ext: ".log", del: true}, extNoDelete: ".gz", nDelete: 0, nNoDelete: 10,
+		{name: "DeleteExtensionNoMatch", cfg: config{ext: []string{".log"}, del: true}, extNoDelete: ".gz", nDelete: 0, nNoDelete: 10,
 			expected: ""},
-		{name: "DeleteExtensionMatch", cfg: config{ext: ".log", del: true}, extNoDelete: "", nDelete: 10, nNoDelete: 0,
+		{name: "DeleteExtensionMatch", cfg: config{ext: []string{".log"}, del: true}, extNoDelete: "", nDelete: 10, nNoDelete: 0,
 			expected: ""},
-		{name: "DeleteExtensionMixed", cfg: config{ext: ".log", del: true}, extNoDelete: ".gz", nDelete: 5, nNoDelete: 5,
+		{name: "DeleteExtensionMixed", cfg: config{ext: []string{".log"}, del: true}, extNoDelete: ".gz", nDelete: 5, nNoDelete: 5,
 			expected: ""},
 	}
 
@@ -127,6 +128,66 @@ func TestRunDelExtension(t *testing.T) {
 				t.Errorf("expected %d log lines, got %d instead", expectedLogLines, len(lines))
 			}
 
+		})
+	}
+}
+
+func TestRunArchive(t *testing.T) {
+	testCases := []struct {
+		name         string
+		cfg          config
+		extNoArchive string
+		nArchive     int
+		nNoArchive   int
+	}{
+		{name: "ArchiveExtensionNoMatch", cfg: config{ext: []string{".log"}}, extNoArchive: ".gz", nArchive: 0, nNoArchive: 10},
+		{name: "ArchveExtensionMatch", cfg: config{ext: []string{".log"}}, extNoArchive: "", nArchive: 10, nNoArchive: 0},
+		{name: "ArchiveExtensionMixed", cfg: config{ext: []string{".log"}}, extNoArchive: ".gz", nArchive: 5, nNoArchive: 5},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			// a buffer for th archive output
+			var buffer bytes.Buffer
+
+			// create tempDirs for runArchive testing
+			tempDir, cleanup := createTempDir(t, map[string]int{
+				testCase.cfg.ext:      testCase.nArchive,
+				testCase.extNoArchive: testCase.nNoArchive,
+			})
+			defer cleanup()
+
+			archiveDir, cleanupArchive := createTempDir(t, nil)
+			defer cleanupArchive()
+
+			testCase.cfg.archive = archiveDir
+
+			if err := run(tempDir, &buffer, testCase.cfg); err != nil {
+				t.Fatal(err)
+			}
+
+			pattern := filepath.Join(tempDir, fmt.Sprintf("*%s", testCase.cfg.ext))
+			expectedFiles, err := filepath.Glob(pattern)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			expectedOutput := strings.Join(expectedFiles, "\n")
+
+			result := strings.TrimSpace(buffer.String())
+
+			if expectedOutput != result {
+				t.Errorf("expected %q, got %q instead\n", expectedOutput, result)
+			}
+
+			filesArchived, err := os.ReadDir(archiveDir)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if len(filesArchived) != testCase.nArchive {
+				t.Errorf("expected %d archived, got %d instead\n", testCase.nArchive, len(filesArchived))
+			}
 		})
 	}
 }
